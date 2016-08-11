bc24eeb2e2e4d7f0f629b8db72595260a131e9d7
Miika Sikala <miikasikala96@gmail.com>
2016-08-12 00:56:08 +0300

    Constant based config
    Added mainly view regarded paths

--- ArtomiSys/config/config.php
+++ ArtomiSys/config/config.php
@@ -1,5 +1,12 @@
 <?php
 
-return [
-	// config data here
-]
+/* GENERAL */
+
+define('APP_NAME', 'ArtomiSys');
+
+
+/* PATHS */
+
+define('PATH_TO_TEMPLATES', 'ArtomiSys/Views/templates');
+define('PATH_TO_SHEETS', 'ArtomiSys/Views');
+define('PATH_TO_SNIPPETS', 'ArtomiSys/Views/snippets');
