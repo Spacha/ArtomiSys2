2367aeb513f2d1fc91b707b421302130a6f10cd4
Miika Sikala <miikasikala96@gmail.com>
2016-08-17 01:07:51 +0300

    Add underline prefix to special view folders

--- ArtomiSys/config/config.php
+++ ArtomiSys/config/config.php
@@ -7,9 +7,13 @@ define('APP_NAME', 'ArtomiSys');
 define('DATE_DEFAULT_TIMEZONE', 'Europe/Helsinki');
 define('DATE_FORM', 'j.m.y \&\m\d\a\s\h; h:i');
 
-
 /* PATHS */
 
-define('PATH_TO_TEMPLATES', 'ArtomiSys/Views/templates');
+define('PATH_TO_TEMPLATES', 'ArtomiSys/Views/_templates');
 define('PATH_TO_SHEETS', 'ArtomiSys/Views');
-define('PATH_TO_SNIPPETS', 'ArtomiSys/Views/snippets');
+define('PATH_TO_SNIPPETS', 'ArtomiSys/Views/_snippets');
+
+/* FILES */
+
+define('UPLOAD_DESTINATION', 'public/uploads/products');
+define('UPLOAD_MAX_SIZE', 1000000);
