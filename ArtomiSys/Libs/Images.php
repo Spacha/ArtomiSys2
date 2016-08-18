bdf503f8bd2603e9aca4cad3ad3253958a43dc9b
Miika Sikala <miikasikala96@gmail.com>
2016-08-18 23:58:20 +0300

    Methods changed as static for practical reasons

--- ArtomiSys/Libs/Images.php
+++ ArtomiSys/Libs/Images.php
@@ -12,15 +12,14 @@ class Images
 {
 	const UPLOAD_DESTINATION = PATH_ROOT . '/' . UPLOAD_DESTINATION;
 
-	protected $allowed_exts = ['jpg','jpeg','png','gif'];
+	protected static $allowed_exts = ['jpg','jpeg','png','gif'];
 
 	/**
 	* Upload images to destination and return array containing image names
 	* @param array images you want to upload
-	* @param uniqid you want to name your image by
 	* @return array containing names of successfully uploaded images
 	*/
-	public function upload(array $images, $uniqid)
+	public static function upload(array $images)
 	{
 		$uploaded = [];
 
@@ -30,7 +29,7 @@ class Images
 		for($i = 0; $i < count($images['name']); $i++) {
 
 			// check if image is real or does it even exist
-			if (!isset($images['name']) || !getImageSize($images['tmp_name'][$i])) {
+			if (!isset($images['name'][$i]) || !getImageSize($images['tmp_name'][$i])) {
 				// $this->throwMessage('Something went wrong while uploading the image :(', 2);
 				// die('Oops!');
 				continue;
@@ -44,12 +43,11 @@ class Images
 			// $ext = end($tmp);
 			$ext = pathinfo($images['name'][$i])['extension'];
 
-			// create uniqid with prefix $uniqid
-			$basename = uniqid($uniqid.'_').'.'.$ext;
+			$basename = 'img_'. date("Y-m-d") .'-'. uniqid() .'.'.$ext;
 			$new_name = PATH_ROOT .'/'. UPLOAD_DESTINATION .'/'. $basename;
 
 			// upload the image
-			if (!in_array(strtolower($ext), $this->allowed_exts)) {
+			if (!in_array(strtolower($ext), self::$allowed_exts)) {
 				// message, type[1 = neutral, 2 = critical]
 				// $this->throwMessage('Image type not supported!', 2);
 				// die('Image type not supported');
@@ -70,7 +68,7 @@ class Images
 	* @param string path you want the images to remove from
 	* @return array containing names of successfully removed images
 	*/
-	public function delete(array $images, $path = self::UPLOAD_DESTINATION)
+	public static function delete(array $images, $path = self::UPLOAD_DESTINATION)
 	{
 		$removed = [];
 
