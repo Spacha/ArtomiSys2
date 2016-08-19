d00d241182515200b496fc1462f608f4a31fa830
Miika Sikala <miikasikala96@gmail.com>
2016-08-19 18:56:16 +0300

    extractImgsStr() method added

--- ArtomiSys/Libs/Helper.php
+++ ArtomiSys/Libs/Helper.php
@@ -12,11 +12,30 @@ class Helper
 {
 	/**
 	* Trims extra spaces after and before string, useful with input fields
-	* @param string str validatable string
-	* @return string validated string 
+	* @param {string} str validatable string
+	* @return {string} validated string 
 	*/
 	public static function validateInput(string $str)
 	{
 		return trim(htmlspecialchars($str));
+	}
+
+	/**
+	* Explodes a string into image names
+	* @param {string} containing image names (separated by ',')
+	* @return {array} image names
+	*/
+	public static function extractImgsStr(string $str)
+	{
+		if (strlen($str) <= 0) {
+			return array();
+		}
+		
+		$imgs = explode(',', $str);
+		$imgs = array_map(
+			function($a){ return trim($a, ', '); },
+			$imgs);
+
+		return $imgs;
 	}
 }
